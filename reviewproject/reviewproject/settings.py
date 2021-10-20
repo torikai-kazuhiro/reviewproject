b7ca13645b54abbe97789e84948ec0e8284b7989
torikai-kazuhiro <79391154+torikai-kazuhiro@users.noreply.github.com>
2021-10-20 19:16:58 +0900

    detail

--- reviewproject/reviewproject/settings.py
+++ reviewproject/reviewproject/settings.py
@@ -118,10 +118,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfile/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/image/'
+
+LOGIN_URL = '/login/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
